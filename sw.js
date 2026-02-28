d73295334d7b203fa95d1e1c93cb2146c7f6c7b3
KEMO-omar <kareemguinness@gmail.com>
2026-02-28 06:11:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('fetch', function(event) {
-  // ده بيخلي المتصفح يصدق إن موقعك "تطبيق" حقيقي
+const CACHE_NAME = 'kramlat-v1';
+const assets = [
+  './',
+  './index.html',
+  './logo.png',
+  './13.png'
+];
+
+// تثبيت السيرفس وركر
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// تشغيل الطلبات حتى لو مفيش إنترنت
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
